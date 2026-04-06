6081ed5c2e9c9eec72b13a11f83fb1508290015b
Vazq-Leon <vazquez.leon2004@icloud.com>
2026-04-06 14:19:38 -0600

    Frontend listo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { TerrenosProvider } from './context/TerrenosContext'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <TerrenosProvider>
+        <App />
+      </TerrenosProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
